b53725fb094f6514434ca4573e0decc391a0ef5f
Renne <kirahlynnst@gmail.com>
2025-03-08 17:47:59 +0700

    Bug fix#3 - revise candidate no. validation

--- validate.js
+++ validate.js
@@ -32,27 +32,28 @@ function checkSID() {
 }
 
 function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
+	let candi = (document.getElementById("candi").value).trim();
+  
+	if (!isNumeric(candi) || !(candi >= 1 && candi <= 10)) {
+	  return true;
+	} else {
 	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
 	}
-}
+  }
+  
+  function validateForm(){
+	  if(!checkSID()){
+		alert("Invalid value for Student ID!");
+		document.getElementById("sid").focus();
+		return false;
+	  }else{
+		  if(checkCandiNo()){
+			alert("Invalid value for Candidate No!!");
+			document.getElementById("candi").focus();
+			return false;
+		  }else{
+			  alert("Your input data passes validation!!");
+			  return true;
+		  }
+	  }
+  }
